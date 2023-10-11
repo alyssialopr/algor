641ef89a6df1b5204d51b31936e835dbbf0e7a98
Alyssia Lorsold Pradon <alyssia.pradon@gmail.com>
2023-10-11 21:35:06 +0200

    Totalité du code mais quelques bugs

--- exotaxi/taxi.js
+++ exotaxi/taxi.js
@@ -16,3 +16,32 @@ const taxi = [
     changeTaxi = 0,
 ]
 
+function JouerPlaylist(){
+    const indexmusic = Math.floor(Math.random() * playlist.length);
+    const play = playlist[indexmusic];
+    console.log(
+      `La musique en cours est ${play}, Il reste ${taxi.feuxRouges} de feux rouges`
+    );
+    return play;
+}
+
+for (let i = 0; i < taxi.feuxRouges; i++) { 
+    const MusiqueJouee = JouerPlaylist;
+    if (MusiqueJouee === "Anissa - Wejdene") {
+      personnage.santeMent -= 1;
+      taxi.changeTaxi += 1;
+      console.log(
+        `Changement de taxi ! Santé mentale de ${personnage.prenom} : ${personnage.santeMent}`
+      );
+    }
+    if (personnage.santeMent === 0) {
+    console.log(`${personnage.prenom} est devenu fou ! EXPLOSION !!`);
+    break;
+    }
+
+    if (personnage.santeMentale > 0) {
+      console.log(
+        `${personnage.prenom} est bien arrivé à destination avec ${taxi.changeTaxi} changements de taxi.`
+      );
+    }
+}
