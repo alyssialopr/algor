f862f944b93d5e1d2f107dd1ae5e58e8de767ae7
Alyssia Lorsold Pradon <alyssia.pradon@gmail.com>
2023-10-11 21:17:20 +0200

    Debut exercice taxi !!

---
+++ exotaxi/taxi.js
@@ -0,0 +1,18 @@
+const playlist = [
+    "Anissa - Wejdene",
+    "Empire State of Mind - Alicia Keys",
+    "Golden - Harry Styles",
+    "Cruel Summer - Taylor Swift",
+    "Story of my Life - One Direction"];
+    
+const personnage = [
+    prenom = "John",
+    santeMent = 10,
+];
+
+const taxi = [
+    radio = playlist,
+    feuxRouges = 30,
+    changeTaxi = 0,
+]
+
